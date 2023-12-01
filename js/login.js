18f5ac36675642023c13d449ab87626ab027e21c
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2023-12-01 19:46:48 +0100

    agg

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 const pass = "Alleluja2024";
-var Cookies2 = Cookies.noConflict()
+var Cookies2 = Cookies.noConflict();
+Cookies2.set("Login", "False");
 
 if(Cookies2.get("Login")=="True"){
     window.location.replace("home.html");
