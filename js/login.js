5f58bdc6630db6fd0428620468e177dbf2ab90e5
priemguardian <79649375+ZELA2000@users.noreply.github.com>
2023-12-14 13:03:45 +0100

    Add enter key functionality to login form

--- js/login.js
+++ js/login.js
@@ -17,4 +17,19 @@ $(document).ready(function(){
             alert("Password non corretta"); 
         }
     });
+    window.addEventListener("keyup", (enter) => {
+        switch(enter.key){
+            case "Enter":
+                let passGet = document.getElementById("password").value;
+                if(passGet == pass){
+                    Cookies2.set("Login", "True", { sameSite: 'strict' });
+                    window.location.replace("home.html");
+                }
+                else{
+                    $("#password").css({"border-color":"red"});
+                    alert("Password non corretta"); 
+                }
+                break;
+        }
+    })
 });
